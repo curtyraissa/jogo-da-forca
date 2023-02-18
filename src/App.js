006a90751a5393c4d1f0c47f29a5b9c7f6f79003
curtyraissa <raissacurty@gmail.com>
2023-02-17 23:23:26 -0300

    import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Jogo } from "./components/Jogo";
+import { Letras } from "./components/Letras";
+
 export default function App() {
   return (
     <>
-    App
+    <Jogo />
+    <Letras />
     </>
   );
 }
