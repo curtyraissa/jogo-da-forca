de11ccf5046aa26b57a74bc1b6f73d2d851668f8
curtyraissa <raissacurty@gmail.com>
2023-02-19 20:35:54 -0300

    criação do main

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { Jogo } from "./components/Jogo/Jogo";
+import { Jogo } from "./components/Jogo";
 import { Letras } from "./components/Letras";
 import palavras from "./palavras";
 
 export default function App() {
   const objJogo = [
     { src: "./assets/forca0.png", alt: "Forca" },
-    { src: "./assets/forca1.png", alt: "Forca" },
+    /*{ src: "./assets/forca1.png", alt: "Forca" },
     { src: "./assets/forca2.png", alt: "Forca" },
     { src: "./assets/forca3.png", alt: "Forca" },
     { src: "./assets/forca4.png", alt: "Forca" },
     { src: "./assets/forca5.png", alt: "Forca" },
-    { src: "./assets/forca6.png", alt: "Forca" }
+    { src: "./assets/forca6.png", alt: "Forca" }*/
   ]
 
   return (
-    <>
+    <main>
       {objJogo.map(i =>
         <Jogo key={i.alt} src={i.src} />
       )}
 
       <Letras />
       
-    </>
+    </main>
   );
 }
 
