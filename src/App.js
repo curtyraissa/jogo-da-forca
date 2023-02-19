bee4ccfb8049504d730a902c101fd4e0343c9de8
curtyraissa <raissacurty@gmail.com>
2023-02-19 18:32:36 -0300

    importado array de palavras

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { Jogo } from "./components/Jogo";
+import { Jogo } from "./components/Jogo/Jogo";
 import { Letras } from "./components/Letras";
+import palavras from "./palavras";
 
 export default function App() {
+  const objJogo = [
+    { src: "./assets/forca0.png", alt: "Forca" },
+    { src: "./assets/forca1.png", alt: "Forca" },
+    { src: "./assets/forca2.png", alt: "Forca" },
+    { src: "./assets/forca3.png", alt: "Forca" },
+    { src: "./assets/forca4.png", alt: "Forca" },
+    { src: "./assets/forca5.png", alt: "Forca" },
+    { src: "./assets/forca6.png", alt: "Forca" }
+  ]
+
   return (
     <>
-    <Jogo />
-    <Letras />
+      {objJogo.map(i =>
+        <Jogo key={i.alt} src={i.src} />
+      )}
+
+      <Letras />
+      
     </>
   );
 }
--- src/components/Jogo.js
+++
@@ -1,7 +0,0 @@
-export const Jogo = () =>{
-    return(
-        <>
-        Jogo
-        </>
-    )
-}
