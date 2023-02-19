18c8a3d9b3dbabd9768a5f35cb3eacad00aa419a
curtyraissa <raissacurty@gmail.com>
2023-02-19 20:35:00 -0300

    complemento do componente jogo e estilização

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,9 +1,15 @@
-import "./Jogo.css"
-
 export const Jogo = (props) => {
   return (
     <div className="jogo-container">
-      <img src={props.src} alt="forca" />
+
+      <div className="jogo-left">
+        <img src={props.src} alt="forca" />
+      </div>
+
+      <div className="jogo-right">
+        <button><p className="jogo-button">Escolher Palavra</p></button>
+        <h1>quarentena</h1>
+      </div>
     </div>
   )
 }
