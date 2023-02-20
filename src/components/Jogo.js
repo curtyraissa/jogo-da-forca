f87a0414d4da2279904746211cd0a95b4f9a8b22
curtyraissa <raissacurty@gmail.com>
2023-02-20 19:43:55 -0300

    adicionado props e estilizacao da palavra no jogo

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,14 +1,20 @@
-export const Jogo = (props) => {
+export const Jogo = ({ erro, mostrar, onClick, disabled, perdeu, ganhou }) => {
+  const objJogo = ["../assets/forca0.png", "../assets/forca1.png", "../assets/forca2.png", "../assets/forca3.png", "../assets/forca4.png", "../assets/forca5.png", "../assets/forca6.png"]
+
   return (
     <div className="jogo-container">
 
       <div className="jogo-left">
-        <img src={props.src} alt="forca" />
+        <img src={objJogo[erro]} alt="forca" />
       </div>
 
       <div className="jogo-right">
-        <button><p className="jogo-button">Escolher Palavra</p></button>
-        <h1>quarentena</h1>
+        <button onClick={onClick}>
+          <p className="jogo-button">Escolher Palavra</p>
+        </button>
+        <h1 style={perdeu ? ganhou ? { color: "green" } : { color: "red" } : { color: "black" }}>
+          {(!disabled || perdeu) ? (mostrar ? mostrar.join(" ") : "") : ""}
+        </h1>
       </div>
     </div>
   )
