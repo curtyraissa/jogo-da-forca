432c7bd5a129f108eac1dde871a2d9ca27d96c7e
curtyraissa <raissacurty@gmail.com>
2023-02-20 19:45:33 -0300

    add desestruturacao  e onclick

--- src/components/Letras.js
+++ src/components/Letras.js
@@ -1,11 +1,12 @@
-export const Letras = (props) => {
+export const Letras = ({disabled, letrasClick, handleClick}) => {
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   return (
     <div className="letras-container">
       <div className="letra">
         {alfabeto.map(l => {
-          return <button key={l} disabled={props.disabled}>
+          const temLetra = letrasClick.includes(l);
+          return <button key={l} onClick={() => handleClick(l)} disabled={disabled || temLetra}>
             {l.toUpperCase()}
           </button>
         })}
